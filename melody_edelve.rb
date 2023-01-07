b1f6c5d402d76c6c69681a3464482d95e1f950fe
Edgar Delgado Vega <106992045+edelveart@users.noreply.github.com>
2023-01-06 23:47:34 -0500

    Melody Edelve
    Function to write a melody in the form [note_1, duration_, note_2, duration_2, ...]

---
+++ melody_edelve.rb
@@ -0,0 +1,10 @@
+
+define :melody_edelve do | note_and_duration |
+  _melody = note_and_duration.ring
+ dur_melody = (_melody.length/2.0)
+ tick_reset
+ dur_melody.times do   
+     play _melody.tick
+     sleep _melody.tick 
+ end
+end
