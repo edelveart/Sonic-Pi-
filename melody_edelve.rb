240581dbd77c01d2162481753f9c45df08c327ce
Edgar Delgado Vega <106992045+edelveart@users.noreply.github.com>
2023-01-06 23:49:29 -0500

    Update melody_edelve.rb

--- melody_edelve.rb
+++ melody_edelve.rb
@@ -1,10 +1,14 @@
 
 define :melody_edelve do | note_and_duration |
+  
   _melody = note_and_duration.ring
- dur_melody = (_melody.length/2.0)
- tick_reset
- dur_melody.times do   
-     play _melody.tick
-     sleep _melody.tick 
- end
+  dur_melody = (_melody.length/2.0)
+  
+  tick_reset
+  
+  dur_melody.times do   
+    play _melody.tick
+    sleep _melody.tick 
+  end
+
 end
